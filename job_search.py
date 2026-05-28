62b7c63851b6c50a8d656f846a4cada966abdf5c
Shaq <nycshaq@gmail.com>
2026-05-28 18:27:43 -0400

    remove screenshots

--- job_search.py
+++ job_search.py
@@ -15,20 +15,39 @@ response = requests.get(url, params=params)
 date = datetime
 data = response.json()
 
+seen_urls = set()
+
+
+try:
+    with open("seen_urls.txt", "r") as f:
+        seen_urls = set(f.read().splitlines())
+except FileNotFoundError:
+    seen_urls = set()
+
+
 for job in data["results"]:
     if job.get("salary_min", 0) >= 130000:
-        print(job["title"])
-        print(job["company"]["display_name"])
-        print(job["redirect_url"])
-        print(f"Salary: ${job.get('salary_min', 'Not listed'):,.0f} - ${job.get('salary_max', 'Not listed'):,.0f}")
-        clean_date = job["created"].removesuffix("Z") 
-        parsed = datetime.strptime(clean_date, "%Y-%m-%dT%H:%M:%S")
-        print(parsed.strftime("%B %d, %Y"))
-        print("---")
-        with open ("jobs_log.txt", "a") as f:
-            f.write(job["title"]+ "\n")
-            f.write(job["company"]["display_name"]+ "\n")
-            f.write(job["redirect_url"]+ "\n")
-            f.write(f"Salary: ${job.get('salary_min', 'Not listed'):,.0f} - ${job.get('salary_max', 'Not listed'):,.0f}"+ "\n")
-            f.write(parsed.strftime("%B %d, %Y")+ "\n")
-            f.write("---"+ "\n")
+        url = job["redirect_url"]
+
+        if url not in seen_urls:
+            print(job["title"])
+            print(job["company"]["display_name"])
+            print(job["redirect_url"])
+            print(f"Salary: ${job.get('salary_min', 'Not listed'):,.0f} - ${job.get('salary_max', 'Not listed'):,.0f}")
+            clean_date = job["created"].removesuffix("Z") 
+            parsed = datetime.strptime(clean_date, "%Y-%m-%dT%H:%M:%S")
+            print(parsed.strftime("%B %d, %Y"))
+            print("---")
+            seen_urls.add(url)
+
+            with open ("jobs_log.txt", "a") as f:
+                f.write(job["title"]+ "\n")
+                f.write(job["company"]["display_name"]+ "\n")
+                f.write(job["redirect_url"]+ "\n")
+                f.write(f"Salary: ${job.get('salary_min', 'Not listed'):,.0f} - ${job.get('salary_max', 'Not listed'):,.0f}"+ "\n")
+                f.write(parsed.strftime("%B %d, %Y")+ "\n")
+                f.write("---"+ "\n")
+
+with open("seen_urls.txt", "w") as f:
+     for url in seen_urls:
+         f.write(url + "\n")
